d6c9ac6fabd6d2af6941c700aa5a960ce4b4685c
Ryan Verner <ryan.verner@gmail.com>
2015-12-30 14:01:17 +1100

    Added working voctomix-hdmi2usb.sh

--- basic/voctomix-hdmi2usb.sh
+++ basic/voctomix-hdmi2usb.sh
@@ -1,8 +1,19 @@
-# gstreamer 1.6
-
-#gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,width=1280,height=720 ! capssetter caps='image/jpeg,width=1280,height=720,framerate=25/1' ! jpegdec ! videoconvert ! matroskamux ! tcpclientsink port=10000 host=localhost
-
-gst-launch-1.0 v4l2src device=/dev/video1 ! image/jpeg,width=1280,height=720 ! capssetter caps='image/jpeg,width=1280,height=720,framerate=25/1' ! jpegdec ! videoconvert ! matroskamux name=mux alsasrc device='hw:0,0' ! audio/x-raw,channels=2,rate=48000 ! audioconvert ! vorbisenc ! queue ! mux. mux. ! queue max-size-bytes=100000000 max-size-time=0 ! tcpclientsink port=10000 host=localhost
-
-
+#!/bin/bash
+    gst-launch-1.0 \
+        v4l2src device=/dev/video1 !\
+            image/jpeg,width=1280,height=720 !\
+            jpegdec !\
+            videoconvert !\
+            videorate !\
+            video/x-raw,format=I420,width=1280,height=720,framerate=25/1,pixel-aspect-ratio=1/1 !\
+            queue !\
+            mux. \
+        \
+        alsasrc device='hw:0,0' !\
+            audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
+            queue !\
+            mux. \
+        \
+        matroskamux name=mux !\
+            tcpclientsink port=10000 host=localhost
 
