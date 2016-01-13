2e5542a5d77b65504deebf156576434b948b4f43
Ryan Verner <ryan.verner@gmail.com>
2016-01-13 19:27:34 +1100

    Fix issue with alsa clock causing drift/slowdown

--- ryan/lca/hdmi2usb-v4l.sh
+++ ryan/lca/hdmi2usb-v4l.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
     gst-launch-1.0 \
-        v4l2src device=/dev/video0 !\
+        v4l2src device=/dev/video1 !\
             image/jpeg,width=1280,height=720 !\
             jpegdec !\
             videoconvert !\
@@ -9,11 +10,11 @@
             queue !\
             mux. \
         \
-        alsasrc device='hw:1,0' !\
+        alsasrc device='hw:1,0' provide-clock=false !\
             audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
             queue !\
             mux. \
         \
         matroskamux name=mux !\
-            tcpclientsink port=10000 host=localhost
+            tcpclientsink port=10001 host=192.168.227.136
 
