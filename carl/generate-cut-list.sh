2cc681606e24b4b11d35c68a610c4e0768cdf7f7
Carl Karsten <carl@personnelware.com>
2016-01-24 19:21:28 +1100

    use fixed version of generate-cut-list.py until vocto PR is merged.

--- carl/generate-cut-list.sh
+++ carl/generate-cut-list.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -ex
 
+
+dir=~/Videos/veyepar/lca/lca2016/dv/$HOSTNAME/$(date +%Y-%m-%d)
+mkdir -p $dir
+
 cd ~/lca/video-scripts/carl
 
-~/lca/voctomix/example-scripts/control-server/generate-cut-list.py \
-  | tee --append cut-list.txt
+# this can go back when ...flush() PR gets merged 
+# ~/lca/voctomix/example-scripts/control-server/generate-cut-list.py \
+./generate-cut-list.py \
+  | tee --append $dir/cut-list.log
 
 
 
