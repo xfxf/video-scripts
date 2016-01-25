cd67763c0fa8277a8f128dce96ca49ebe8549303
Carl Karsten <carl@personnelware.com>
2016-01-25 23:33:25 +1100

    simple grabber in a loop for slave box.

---
+++ carl/grabber.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+
+# hdmi2usb, generated audio, local display (including fps)
+# used for screen grabber, loops and sends to core
+
+while true; do
+
+    gst-launch-1.0 -v \
+        v4l2src device=$HDMI2USB !\
+            image/jpeg,width=1280,height=720 !\
+            jpegdec !\
+            videoconvert !\
+            videorate !\
+         tee name=t ! queue ! \
+            videoconvert ! fpsdisplaysink t. ! \
+            video/x-raw,format=I420,width=1280,height=720,framerate=30/1,pixel-aspect-ratio=1/1 !\
+            queue !\
+            mux. \
+        \
+        audiotestsrc !\
+            audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
+            queue !\
+            mux. \
+        \
+        matroskamux name=mux !\
+            tcpclientsink port=10000 host=$VOC_CORE
+
+    sleep 1
+
+done
