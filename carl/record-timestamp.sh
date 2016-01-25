f79f27c863703bde64a016b2563846875c944f12
Carl Karsten <carl@personnelware.com>
2016-01-25 14:51:56 +1100

    10 min chunks

--- carl/record-timestamp.sh
+++ carl/record-timestamp.sh
@@ -11,5 +11,6 @@ ffmpeg \
 	-map 0:v -c:v:0 mpeg2video -pix_fmt:v:0 yuv422p -qscale:v:0 2 -qmin:v:0 2 -qmax:v:0 7 -keyint_min 0 -bf:0 0 -g:0 0 -intra:0 -maxrate:0 90M \
 	-map 0:a -c:a:0 mp2 -b:a:0 192k -ac:a:0 2 -ar:a:0 48000 \
 	-flags +global_header -flags +ilme+ildct \
-	-f segment -segment_time 360 -segment_format mpegts \
+	-f segment -segment_time 600 \
+        -segment_format mpegts \
         -strftime 1 "$dir/%Y-%m-%d/%H_%M_%S.ts"
