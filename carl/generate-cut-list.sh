25d03eea7ab50f662fffea34bbb5a4f07fe6664b
Carl Karsten <carl@personnelware.com>
2016-01-25 18:33:38 +1100

    Vocto .flush PR merged, no need for local patched version.

--- carl/generate-cut-list.sh
+++ carl/generate-cut-list.sh
@@ -6,9 +6,7 @@ mkdir -p $dir
 
 cd ~/lca/video-scripts/carl
 
-# this can go back when ...flush() PR gets merged 
-# ~/lca/voctomix/example-scripts/control-server/generate-cut-list.py \
-./generate-cut-list.py \
+~/lca/voctomix/example-scripts/control-server/generate-cut-list.py \
   | tee --append $dir/cut-list.log
 
 
