d7420c93738a3f73d49745d71d589ff37e966493
Carl Karsten <carl@personnelware.com>
2016-01-26 19:51:18 +1100

    use hu-conf.py in grab-loop.sh

--- carl/grab-loop.sh
+++ carl/grab-loop.sh
@@ -6,7 +6,10 @@ cd ~/lca/video-scripts/carl/
 
 while true; do
 
-    ./hu-conf.sh
+    python hu-conf.sh /dev/ttyVIZ0 hdmi2usb.cmd
+    # flterm --port /dev/ttyVIZ0 --speed 115200 < hdmi2usb.cmd
+    # flterm --port /dev/ttyACM0 --speed 115200 < hdmi2usb.cmd
+
     ./grabber.sh 1
 
     sleep 1
