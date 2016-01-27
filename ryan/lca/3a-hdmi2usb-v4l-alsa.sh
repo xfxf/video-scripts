94725596bd5da8ae17bc9cbde25a5b35a28b691d
Ryan Verner <ryan.verner@gmail.com>
2016-01-27 11:23:43 +1100

    Add 3x firewire-capture scripts - has not been tested

--- ryan/lca/3a-hdmi2usb-v4l-alsa.sh
+++ ryan/lca/3a-hdmi2usb-v4l-alsa.sh
@@ -3,17 +3,19 @@
 SRC="$(dirname $(realpath ${BASH_SOURCE[@]}))"
 source $SRC/A-variables.sh
 
+PORT=10001
+HOST=192.168.0.15
+
     gst-launch-1.0 \
-        v4l2src device=$DEVICE !\
-            image/jpeg,width=1280,height=720 !\
-            jpegdec !\
+        videotestsrc pattern=blank !\
             videoconvert !\
             videorate !\
+            videoscale !\
             video/x-raw,format=I420,width=1280,height=720,framerate=30/1,pixel-aspect-ratio=1/1 !\
             queue !\
             mux. \
         \
-        alsasrc device='hw:1,0' !\
+        alsasrc device='hw:1,0' provide-clock=false
             audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
             queue !\
             mux. \
---
+++ ryan/lca/other/firewire-capture/dv-camera-noaudio.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#gst-launch-1.0 hdv1394src ! decodebin ! fpsdisplaysink
+
+#gst-launch-1.0 dv1394src ! decodebin ! videoconvert ! videorate ! fpsdisplaysink sync=false
+
+#gst-launch-1.0 dv1394src ! dvdemux name=demux ! queue ! audioconvert ! fakesink demux. ! queue ! dvdec ! deinterlace ! videoconvert ! videorate ! videoscale ! video/x-raw,format=I420,width=1280,height=720,framerate=30/1,pixel-aspect-ratio=1/1 ! fpsdisplaysink sync=false
+
+export GST_DEBUG=*:3
+export HOST=192.168.0.15
+export PORT=10000
+
+    gst-launch-1.0 \
+        dv1394src !\
+            multiqueue !\
+	    dvdemux name=demux \
+	    demux. !\
+            queue !\
+	    	audioconvert !\
+	    	audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
+	    	queue !\
+		    mux. \
+	    audiotestsrc wave=silence !\
+            audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\ 
+            queue !\
+            mux. \
+	    matroskamux name=mux !\
+          	tcpclientsink port=$PORT host=$HOST \
+        t. !\
+            queue !\
+            fpsdisplaysink sync=false
+
