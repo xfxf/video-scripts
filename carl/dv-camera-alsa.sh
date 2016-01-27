fe1292b2370101e018ba6728492236292df14b14
Carl Karsten <carl@personnelware.com>
2016-01-27 23:46:24 +1100

    leaner dv-alsa source.

--- carl/dv-camera-alsa.sh
+++ carl/dv-camera-alsa.sh
@@ -1,26 +1,22 @@
-#!/bin/bash
+#!/bin/bash -ex
 
     gst-launch-1.0 \
         dv1394src !\
-            multiqueue !\
-	    dvdemux name=demux \
-	    alsasrc device=$VOC_ALSA_DEV  provide-clock=false !\
-            audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
-		    queue !\
-            mux. \
+	    multiqueue !\
+            dvdemux name=demux \
+		alsasrc device=$VOC_ALSA_DEV  provide-clock=false !\
+		audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
+		queue !\
+            	mux. \
 	    demux. !\
-		    dvdec !\
+		dvdec !\
 	    	deinterlace !\
-		    videoconvert !\
+		videoconvert !\
 	    	videorate !\
  	    	videoscale !\
-		    video/x-raw,format=I420,width=1280,height=720,framerate=30/1,pixel-aspect-ratio=1/1 !\
-		    tee name=t !\
-		    queue !\
+		video/x-raw,format=I420,width=1280,height=720,framerate=30/1,pixel-aspect-ratio=1/1 !\
+		queue !\
 		mux. \
             matroskamux name=mux !\
-          	tcpclientsink port=1000$1 host=$VOC_CORE \
-        t. !\
-            queue !\
-            fpsdisplaysink sync=false
+        tcpclientsink port=1000$1 host=$VOC_CORE 
 
