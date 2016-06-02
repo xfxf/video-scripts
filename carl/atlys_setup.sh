fd7166167d3a7c4b8b90fe743a4d9b1eb77e7c9c
Carl Karsten <carl@personnelware.com>
2016-06-02 07:42:11 -0500

    Notes on how to flash Opsis

--- carl/atlys_setup.sh
+++ carl/atlys_setup.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -x
 
-# sudo apt-add-repository --yes ppa:timvideos/fpga-support
-# sudo apt-add-repository --yes ppa:timvideos/dvswitch
-# sudo apt-add-repository --yes ppa:carlfk/ppa
-# sudo apt-get update && sudo apt-get --assume-yes upgrade
-# sudo apt-get --assume-yes install vizzini-dkms
-# sudo apt-get --assume-yes install openocd fxload flterm
-# sudo apt-get --assume-yes install libftdi1 libusb-dev libftdi-dev ixo-usb-jtag
-# sudo apt-get --assume-yes install dvsource-v4l2-other gstreamer1.0-libav
+v=v0.0.0-606-g3a5f323 # doesn't boot - nothing when usb plugged in or lsusb
+v=v0.0.0-623-g5ac1879 # no boot
+v=v0.0.0-632-g342039c # uvc color bars, no ttyACM0, 16c0:06ad Van Ooijen
+v=v0.0.0-635-g7fe1321 # no boot
+v=v0.0.0-639-g956a8b3 # noise
+v=v0.0.0-639-g956a8b3 # uvc bars, no ttyACM0, 16c0:06ad Van Ooijen
+v=v0.0.0-650-g5721f74 # ditto (can't seem to get it out of jtag mode)
+v=v0.0.0-654-gbca243b # ditto
+v=v0.0.0-658-g0d326d4 # ditto
+v=v0.0.0-663-g54d888a # ditto 
+
+v=v0.0.0-699-gb505665  # noise on hdmi out and encoder stream
+                       # atlys - dirty bars on romboot 
+                       
+
+board=atlys
 
 # git clone https://github.com/timvideos/HDMI2USB-firmware-prebuilt.git
 # cd ~/HDMI2USB-firmware-prebuilt/atlys/firmware/unstable
-cd ~/HDMI2USB-firmware-prebuilt/archive/master/v0.0.0-639-g956a8b3/atlys/hdmi2usb
+cd ~/HDMI2USB-firmware-prebuilt/archive/master/$v/$board/hdmi2usb
 
 # cd ~/lca/video-scripts/carl/atlys-firmware
 
@@ -21,7 +29,7 @@ fxload -B libusb -D vid=0x16c0,pid=0x06ad -t fx2lp -I hdmi2usb.hex
 flterm --port /dev/ttyVIZ0 --speed 115200 
 flterm --port /dev/ttyACM0 --speed 115200
 
-mplayer tv:// -tv driver=v4l2:device=$HDMI2USB
+mplayer tv:// -tv driver=v4l2:device=/dev/video1
 
 echo if you see yourself in a web cam, fix this:
 grep HDMI2USB ~/.bashrc
--- carl/opsis_setup.sh
+++ carl/opsis_setup.sh
@@ -1,11 +1,27 @@
 
-v=v0.0.0-699-gb505665
+v=v0.0.0-431-g717dfa8 # opsis_hdmi2usb-hdmi2usbsoc-opsis.bin does not exist
+v=v0.0.0-639-g956a8b3 # noise
+v=v0.0.0-606-g3a5f323 # doesn't boot - nothing when usb plugged in or lsusb
+v=v0.0.0-632-g342039c # uvc color bars, no ttyACM0, 16c0:06ad Van Ooijen
+v=v0.0.0-635-g7fe1321 # no boot
+v=v0.0.0-639-g956a8b3 # uvc bars, no ttyACM0, 16c0:06ad Van Ooijen
+v=v0.0.0-650-g5721f74 # ditto (can't seem to get it out of jtag mode)
+v=v0.0.0-654-gbca243b # ditto
+v=v0.0.0-658-g0d326d4 # ditto
+v=v0.0.0-663-g54d888a # ditto 
+
+v=v0.0.0-699-gb505665 # noise on hdmi out and encoder stream
+                      # uvc broken stream
+v=v0.0.0-623-g5ac1879 # 
+
+ln -sf ~/src/tv/HDMI2USB-firmware-prebuilt/archive/master/$v/opsis/hdmi2usb
 
 cd /tmp
-
-ln -sf /home/carl/src/tv/HDMI2USB-firmware-prebuilt/archive/master/$v/opsis/hdmi2usb
+ln -sf ~/HDMI2USB-firmware-prebuilt/archive/master/$v/opsis/hdmi2usb
 
 cd
+git clone https://github.com/timvideos/HDMI2USB-firmware-prebuilt.git
+
 git clone https://github.com/mithro/HDMI2USB-mode-switch.git
 cd HDMI2USB-mode-switch
 git checkout opsis-prod
@@ -16,4 +32,8 @@ sudo ./hdmi2usb-mode-switch.py --mode=jtag --verbose
 sudo ./hdmi2usb-mode-switch.py --verbose --flash-gateware /tmp/hdmi2usb/opsis_hdmi2usb-hdmi2usbsoc-opsis.bin
 
 # power cycle to get it out of jtag mode
+
+openocd -f board/numato_opsis.cfg -c 'init; xc6s_print_dna xc6s.tap; jtagspi_init 0 ~/HDMI2USB-mode-switch/flash_proxy/opsis/bscan_spi_xc6slx45t.bit; jtagspi_program /tmp/hdmi2usb/opsis_hdmi2usb-hdmi2usbsoc-opsis.bin 0x0; exit'
+
+mplayer tv:// -tv driver=v4l2:device=/dev/video1
 
