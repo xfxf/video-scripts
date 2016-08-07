98bd2944294b716ecf61792fb48ada915ba2843b
videoteam <videoteam@avserver.private>
2016-08-07 22:45:11 +1000

    Fix ingest.py parameters

--- pycon/grab-loop.sh
+++ pycon/grab-loop.sh
@@ -8,7 +8,7 @@ while true; do
 
     # python hu-conf.py /dev/ttyVIZ0 hdmi2usb.cmd
 
-    ./ingest.py --video-source hdmi2usb --video-dev $HDMI2USB --host $VOC_CORE --port 10001
+    ./ingest.py --video-source hdmi2usb --video-attribs device=$HDMI2USB --host $VOC_CORE --port 10001
 
     sleep 1
 
