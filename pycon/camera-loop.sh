6586b5ec4936861ad59b02e07a75e181da3c1add
videoteam <videoteam@avserver.private>
2016-08-07 23:45:13 +1000

    Fix camera loop script (audio device)

--- pycon/camera-loop.sh
+++ pycon/camera-loop.sh
@@ -8,7 +8,7 @@ while true; do
 
     # python hu-conf.py /dev/ttyVIZ0 hdmi2usb.cmd
 
-    ./ingest.py --video-source hdmi2usb --video-attribs device=$HDMI2USB --audio-source pulse --audio-dev $VOC_PULSE_DEV --host $VOC_CORE --port 10000
+    ./ingest.py --video-source hdmi2usb --video-attribs device=$HDMI2USB --audio-source pulse --audio-attribs device=$VOC_PULSE_DEV --host $VOC_CORE --port 10000
 
     sleep 1
 
