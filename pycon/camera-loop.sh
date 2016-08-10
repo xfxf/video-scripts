a2ac101102dec79e7f7d47037bb8c137ca42d4ce
Ryan Verner <ryan.verner@gmail.com>
2016-08-10 20:34:42 +1000

    Revert camera loop audio delay

--- pycon/camera-loop.sh
+++ pycon/camera-loop.sh
@@ -8,7 +8,7 @@ while true; do
 
     # python hu-conf.py /dev/ttyVIZ0 hdmi2usb.cmd
 
-    ./ingest.py --video-source hdmi2usb --video-attribs device=$HDMI2USB --audio-source alsa --audio-attribs device=$DVS_ALSA_DEV --host $VOC_CORE --port 10000 --video-delay 200
+    ./ingest.py --video-source hdmi2usb --video-attribs device=$HDMI2USB --audio-source alsa --audio-attribs device=$DVS_ALSA_DEV --host $VOC_CORE --port 10000 --audio-delay 1000
 
     sleep 1
 
