b46a209d8651911a1509b615e758be3f5ffbf765
Michael Farrell <micolous+git@gmail.com>
2021-07-17 20:03:38 +1000

    Partially revert "Change AU to US", include security group
    This reverts commit 22e2388f30ac1d2c786fb80e56691dc193f1f736.

--- peer/ec2-pycon-relay-storage.sh
+++ peer/ec2-pycon-relay-storage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 create-volume --availability-zone us-east-2b --no-encrypted --volume-type gp2 --size 250 --tag-specifications "ResourceType=volume,Tags=[{Key=i-type,Value=ingest-store},{Key=i-room,Value=$1},{Key=Name,Value='RTMP Storage $1'}]" 
+aws ec2 create-volume --availability-zone ap-southeast-2b --no-encrypted --volume-type gp2 --size 250 --tag-specifications "ResourceType=volume,Tags=[{Key=i-type,Value=ingest-store},{Key=i-room,Value=$1},{Key=Name,Value='RTMP Storage $1'}]" 
