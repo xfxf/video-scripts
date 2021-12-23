deadd155dc5a2669a2d96f1726acf680b735cc9a
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-12-23 19:48:57 +1100

    fix jitsi/test.sh variables

--- patrick/jitsi/test.sh
+++ patrick/jitsi/test.sh
@@ -3,6 +3,7 @@
 export AWS_DEFAULT_REGION=ap-southeast-2
 export AWS_REGION=$AWS_DEFAULT_REGION
 
+STACK_NAME=jitsi-role
 INSTANCE_PROFILE=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query 'Stacks[0].Outputs[?OutputKey==`InstanceProfile`].OutputValue' --output text)
 SECURITY_GROUP=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query 'Stacks[0].Outputs[?OutputKey==`SecurityGroup`].OutputValue' --output text)
 
@@ -13,5 +14,5 @@ aws ec2 run-instances --image-id "ami-0bf8b986de7e3c7ce" \
   --user-data file://ec2-userdata.yaml \
   --associate-public-ip-address \
   --subnet-id subnet-33af5c55 \
-  --iam-instance-profile "Name=jitsi-role-InstanceProfile-${INSTANCE_PROFILE}" \
+  --iam-instance-profile "Name=${INSTANCE_PROFILE}" \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='$1'},{Key=owner,Value='patrick'},{Key='letsencrypt:email',Value='$2'}]" 
